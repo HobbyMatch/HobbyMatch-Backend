9236fa07c6e94c5e40efaefa57ae6cb7d85a56a0
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-03-10 14:50:11 +0100

    Fixed .gitignore, created project structure, created the hello endpoint and test for it

---
+++ src/main/kotlin/com/github/kkkubakkk/hobbymatchbackend/HobbyMatchBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.github.kkkubakkk.hobbymatchbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HobbyMatchBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<HobbyMatchBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/kkkubakkk/hobbymatchbackend/HobbyMatchBackendApplicationTests.kt
@@ -0,0 +1,24 @@
+package com.github.kkkubakkk.hobbymatchbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HobbyMatchBackendApplicationTests {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `should return Hello, world! on GET request to hello endpoint`() {
+        mockMvc.perform(get("/hello"))
+            .andExpect(status().isOk)
+            .andExpect(content().string("Hello, world!"))
+    }
+}
