41cbbc725b320ec68c1933be97c59439154861c9
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-03-25 12:56:53 +0100

    Add Detekt for static code analysis and update pre-commit hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "1.9.25"
     id("org.jlleitschuh.gradle.ktlint") version "12.2.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.0"
 }
 
 group = "com.github.KKKUBAKKK"
