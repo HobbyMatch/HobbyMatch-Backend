b40e0c0909e5e370e731dd8b28a8bc3b63829a32
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-03-25 13:39:45 +0100

    Add Detekt formatting and CLI plugins to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-cli:1.23.0")
 }
 
 kotlin {
