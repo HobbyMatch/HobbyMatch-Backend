542f702c6b5fe9548643d4eaa8d579a85e4921b0
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-03-29 20:13:28 +0100

    Configure Testcontainers for SQL Server in tests and update application-test.properties

--- src/test/kotlin/com/github/kkkubakkk/hobbymatchbackend/HobbyMatchBackendApplicationTests.kt
+++ src/test/kotlin/com/github/kkkubakkk/hobbymatchbackend/HobbyMatchBackendApplicationTests.kt
@@ -4,14 +4,38 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.testcontainers.containers.MSSQLServerContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Testcontainers
 class HobbyMatchBackendApplicationTests {
+    companion object {
+        @Container
+        val sqlServer = MSSQLServerContainer<Nothing>("mcr.microsoft.com/mssql/server:2019-latest").apply {
+            withPassword("A_Str0ng_Password!")
+            withEnv("ACCEPT_EULA", "Y")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url") { sqlServer.jdbcUrl }
+            registry.add("spring.datasource.username") { sqlServer.username }
+            registry.add("spring.datasource.password") { sqlServer.password }
+        }
+    }
+
     @Autowired
     private lateinit var mockMvc: MockMvc
 
