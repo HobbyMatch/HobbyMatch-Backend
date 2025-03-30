adf12167d763104e4db93862e66fe4f5f9f8fabd
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-03-30 23:54:58 +0200

    Enhance User entity with custom equals, hashCode, and toString methods

--- src/main/kotlin/com/github/kkkubakkk/hobbymatchbackend/user/model/User.kt
+++ src/main/kotlin/com/github/kkkubakkk/hobbymatchbackend/user/model/User.kt
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.hibernate.proxy.HibernateProxy
 
 @Entity
 @Table(name = "users")
@@ -22,4 +23,29 @@ data class User(
     var username: String,
     @Column(name = "email", nullable = false, unique = true, columnDefinition = "VARCHAR(320)")
     var email: String,
-)
+) {
+    final override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null) return false
+        val oEffectiveClass =
+            if (other is HibernateProxy) other.hibernateLazyInitializer.persistentClass else other.javaClass
+        val thisEffectiveClass =
+            if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass else this.javaClass
+        if (thisEffectiveClass != oEffectiveClass) return false
+        other as User
+
+        return id == other.id
+    }
+
+    final override fun hashCode(): Int =
+        if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass.hashCode() else javaClass.hashCode()
+
+    @Override
+    override fun toString(): String =
+        this::class.simpleName +
+            "(  id = $id   ,   " +
+            "firstName = $firstName   ,   " +
+            "lastName = $lastName   ,   " +
+            "username = $username   ,   " +
+            "email = $email )"
+}
