d9d8e587aa2084842e7d49314ae91808b06160b2
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-03-30 09:41:33 +0200

    Implement OAuth2 authentication with custom success and failure handlers

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,13 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 kotlin {
