4cb26e344f6dffe8c6c25a7c51a0ff31daf73aae
SophieKuzmenko <sophie.s.kuzmenko@gmail.com>
2025-04-14 00:28:21 +0300

    test commit

---
+++ src/test/kotlin/com/github/kkkubakkk/hobbymatchbackend/user/service/UserServiceTest.kt
@@ -0,0 +1,11 @@
+package com.github.kkkubakkk.hobbymatchbackend.user.service
+
+import com.github.kkkubakkk.hobbymatchbackend.user.repository.UserRepository
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+
+class UserServiceTest {
+
+    @MockitoBean
+    private lateinit var userRepository: UserRepository
+
+}
