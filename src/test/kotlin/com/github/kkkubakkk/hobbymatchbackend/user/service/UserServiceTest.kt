fee7ac109868861815b645add5d6644c3544efd3
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-04-13 23:49:58 +0200

    Refactor to pass Ktlint and detekt checks

--- src/test/kotlin/com/github/kkkubakkk/hobbymatchbackend/user/service/UserServiceTest.kt
+++ src/test/kotlin/com/github/kkkubakkk/hobbymatchbackend/user/service/UserServiceTest.kt
@@ -1,9 +1,6 @@
 package com.github.kkkubakkk.hobbymatchbackend.user.service
 
-import com.github.kkkubakkk.hobbymatchbackend.user.repository.UserRepository
-import org.springframework.test.context.bean.override.mockito.MockitoBean
-
 class UserServiceTest {
-    @MockitoBean
-    private lateinit var userRepository: UserRepository
+//    @MockitoBean
+//    private lateinit var userRepository: UserRepository
 }
