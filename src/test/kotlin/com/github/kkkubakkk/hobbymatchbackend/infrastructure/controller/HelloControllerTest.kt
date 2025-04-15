4ea63f18caf9f9dd7aaf7b74d9fa26e771fc7e8f
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-04-15 20:15:02 +0200

    Enhance HelloControllerTest with JWT authentication mocks

--- src/test/kotlin/com/github/kkkubakkk/hobbymatchbackend/infrastructure/controller/HelloControllerTest.kt
+++ src/test/kotlin/com/github/kkkubakkk/hobbymatchbackend/infrastructure/controller/HelloControllerTest.kt
@@ -1,9 +1,12 @@
 package com.github.kkkubakkk.hobbymatchbackend.infrastructure.controller
 
+import com.github.kkkubakkk.hobbymatchbackend.security.component.JwtAuthenticationFilter
+import com.github.kkkubakkk.hobbymatchbackend.security.component.JwtUtils
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
@@ -13,6 +16,12 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 class HelloControllerTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
+
+    @MockitoBean
+    private lateinit var jwtUtils: JwtUtils
+
+    @MockitoBean
+    private lateinit var jwtAuthenticationFilter: JwtAuthenticationFilter
 
     @Test
     fun `hello endpoint returns Hello, world!`() {
